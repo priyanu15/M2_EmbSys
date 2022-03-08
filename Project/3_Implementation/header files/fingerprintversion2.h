c8eb0ad5823db91c5a96d6c6f735f73a77698cfd
priyanu15 <priyanutushar36@gmail.com>
2022-03-08 12:49:45 +0530

    Merge branch 'main' of https://github.com/priyanu15/M2_EmbSys

---
+++ Project/3_Implementation/header files/fingerprintversion2.h
@@ -0,0 +1,20 @@
+void setup(void) {
+
+  startFingerprintSensor();
+  display.begin();
+  displayLockScreen(); 
+}
+
+void loop() {
+
+  fingerprintID = getFingerprintID();
+  delay(50);
+  if(fingerprintID == 1)
+  {
+    display.drawBitmap(30,35,icon,60,60,GREEN);
+    delay(2000);
+    displayUnlockedScreen();
+    displayIoanna();
+    delay(5000);
+    display.fillScreen(BLACK);
+    displayLockScreen();
---
+++ Project/3_Implementation/header files/fingrtprint.h
@@ -0,0 +1,15 @@
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <TFT_ILI9163C.h>
+#include <Adafruit_Fingerprint.h>
+#include <SoftwareSerial.h>
+
+// Color definitions
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0  
+#define WHITE   0xFFFF
